51423d00ca43ba14fdeb83304d5ac337795b9938
Oskar <Oskar@DESKTOP-HK7VK3E>
2023-06-10 13:32:35 +0200

    Se mueven los impl a su paquete y se documentan los servicios. - oskar

--- proyectoWeb/src/main/java/com/app/web/servicios/ObjetoskinarmaServicio.java
+++ proyectoWeb/src/main/java/com/app/web/servicios/ObjetoskinarmaServicio.java
@@ -5,12 +5,50 @@ import java.util.List;
 import com.app.web.entidad.Caja;
 import com.app.web.entidad.ObjetoSkinArma;
 
+/**
+ * Interfaz que define los métodos para operar con la entidad ObjetoSkinArma en el sistema.
+ * 
+ * @author: Oskar Stankevicius
+ * @version: 1.0.6v
+ */
 public interface ObjetoskinarmaServicio {
 	
-	public List<ObjetoSkinArma> listarOSA();
-	public ObjetoSkinArma guardarOSA(ObjetoSkinArma osa);
-	public ObjetoSkinArma obtenerOSAPorId(Long id);
-	public ObjetoSkinArma actualizarOSA(ObjetoSkinArma osa);
-	public void eliminarOSA(Long id);
+	/**
+     * Retorna una lista de todos los objetos de skin de arma en el sistema.
+     *
+     * @return Una lista de objetos ObjetoSkinArma.
+     */
+    public List<ObjetoSkinArma> listarOSA();
+
+    /**
+     * Guarda un objeto de skin de arma en el sistema.
+     *
+     * @param osa El objeto de skin de arma a guardar.
+     * @return El objeto de skin de arma guardado.
+     */
+    public ObjetoSkinArma guardarOSA(ObjetoSkinArma osa);
+
+    /**
+     * Obtiene un objeto de skin de arma por su ID.
+     *
+     * @param id El ID del objeto de skin de arma.
+     * @return El objeto de skin de arma encontrado, o null si no se encuentra ningún objeto con ese ID.
+     */
+    public ObjetoSkinArma obtenerOSAPorId(Long id);
+
+    /**
+     * Actualiza un objeto de skin de arma en el sistema.
+     *
+     * @param osa El objeto de skin de arma a actualizar.
+     * @return El objeto de skin de arma actualizado.
+     */
+    public ObjetoSkinArma actualizarOSA(ObjetoSkinArma osa);
+
+    /**
+     * Elimina un objeto de skin de arma del sistema.
+     *
+     * @param id El ID del objeto de skin de arma a eliminar.
+     */
+    public void eliminarOSA(Long id);
 
 }
--- proyectoWeb/src/main/java/com/app/web/servicios/SecurityUserDetailsService.java
+++ proyectoWeb/src/main/java/com/app/web/servicios/SecurityUserDetailsService.java
@@ -8,25 +8,39 @@ import org.springframework.stereotype.Service;
 import com.app.web.repositorios.UserRepository;
 import com.app.web.seguridad.SecurityUser;
 
+/**
+ * Servicio que implementa la interfaz UserDetailsService de Spring Security para cargar los detalles de un usuario durante el proceso de autenticación.
+ * 
+ * @author: Oskar Stankevicius
+ * @version: 1.0.6v
+ */
 @Service
 public class SecurityUserDetailsService implements UserDetailsService{
 
-	
 	private final UserRepository userRepo;
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		var optUser = this.userRepo.findByName(username);
-		if(optUser.isPresent()) {
-			return new SecurityUser(optUser.get());
-		}
-		throw new UsernameNotFoundException("Usuario no encontrado " + username);
-	}
-	public SecurityUserDetailsService(UserRepository userRepo) {
-		super();
-		this.userRepo = userRepo;
-	}
-	
-	
-	
 
+	/**
+     * Constructor de la clase SecurityUserDetailsService.
+     *
+     * @param userRepo El repositorio de usuarios utilizado para buscar usuarios.
+     */
+    public SecurityUserDetailsService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+	
+	/**
+     * Carga los detalles de un usuario por su nombre de usuario.
+     *
+     * @param username El nombre de usuario del usuario.
+     * @return Los detalles del usuario encontrado.
+     * @throws UsernameNotFoundException Si no se encuentra ningún usuario con el nombre de usuario especificado.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var optUser = this.userRepo.findByName(username);
+        if (optUser.isPresent()) {
+            return new SecurityUser(optUser.get());
+        }
+        throw new UsernameNotFoundException("Usuario no encontrado: " + username);
+    }
 }
